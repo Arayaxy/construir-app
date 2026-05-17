b304b195afcd50ed8d6788d685b6d44b8799c420
Arayax <inigoibarluceamoreno@gmail.com>
2026-05-17 18:56:28 +0200

    correccion

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = require("./public/router/rutas");
 
 const app = express();
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
--- src/public/router/rutas.js
+++ src/public/router/rutas.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const router = express.Router("./public/router/rutas");
+const router = express.Router();
 
 router.get('/', (req, res) => {
     res.send("<h1>Práctica JWT</h1>");
