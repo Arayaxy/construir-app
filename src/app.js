cc20c04de3954150ef0d9aebcd85969f1eb2d0c2
Arayax <inigoibarluceamoreno@gmail.com>
2026-05-17 18:52:37 +0200

    correciones

--- src/app.js
+++ src/app.js
@@ -7,10 +7,6 @@ const port = 3000
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
-app.use(express.urlencoded({extended: false}));
-
-app.use(express.json());
-
 app.use("/", router);
 
 app.listen(port, () => {
